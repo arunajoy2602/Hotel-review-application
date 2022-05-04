ffaab748b1d815a8be059028777373d387023dd0
arunajoy2602 <arunajoy2602@gmail.com>
2022-05-04 17:08:41 +0400

    final test

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Button from './Button';
+
+describe('the <Button /> component', () => {
+    const renderer = new ShallowRenderer();
+    // it('should render', () => {
+    //     const children = 'This is a button';
+    //     renderer.render(<Button>{children}</Button>);
+    //     const result = renderer.getRenderOutput();
+    //     expect(result).toMatchSnapshot();
+    // });
+
+    it('should render the correct children', () => {
+        const children = 'This is a button';
+        renderer.render(<Button>{children}</Button>);
+        const component = renderer.getRenderOutput();
+        expect(component.props.children).toEqual(children);
+    });
+
+});
